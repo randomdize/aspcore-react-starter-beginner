ed884b004509b6a306b0419db4a54cbc2c4331dc
Jack Shih <randomdize@gmail.com>
2017-08-08 12:01:02 +0800

    first edition.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+
+namespace aspcore_react_starter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return new PhysicalFileResult(_hostingEnvironment.WebRootPath + "/index.html", "text/html");
+        }
+    }
+}
